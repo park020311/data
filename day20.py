eccd52d40db0cf99eb7505b9a3441f1fc55331fd
park020311 <zxwrtygsnm@naver.com>
2024-02-08 15:43:52 +0900

    v0.8.2 poly

--- day20.py
+++ day20.py
@@ -1,34 +1,33 @@
-def print_poly(f_x) -> str:
-    term = len(f_x) - 1
+def print_poly(f_x,t_x) -> str:
+
     poly_expression = "f(x) = "
 
     for i in range(len(fx)):
+        term = t_x[i]
         coefficient = f_x[i]
-        if coefficient == 0:
-            term = term -1
-            continue
-        elif coefficient >= 0 and i !=0 :
+        if coefficient >= 0 and i !=0 :
             poly_expression = poly_expression + "+"
         poly_expression = poly_expression + f"{coefficient}x^{term} "
-        term = term - 1
+
 
     return poly_expression
 
-def calculation_poly(xVal, f_x):
+def calculation_poly(xVal, f_x,t_x):
     return_value = 0
-    term = len(f_x) - 1  
+
 
     for i in range(len(fx)):
+        term = t_x[i]
         coef = f_x[i]
         return_value += coef * pow(xVal,term)
-        term = term - 1
+
 
     return return_value
-
-fx = [2,3, 4, 0, -9]
-
+tx= [300,20,0]
+fx = [7,-4,5]
+#x=0일때 오류남. 상수일때 x^0 이 붙지 않게 바꿔야함..
 
 if __name__ == "__main__":
 
-    print(print_poly(fx))
-    print(calculation_poly(int(input("X 값--> ")), fx))
+    print(print_poly(fx,tx))
+    print(calculation_poly(int(input("X 값--> ")), fx,tx))
