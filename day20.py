bfda6befe4d5bfa74bc344b5bc9e867c9dae509b
park020311 <zxwrtygsnm@naver.com>
2024-02-08 15:04:25 +0900

    v0.8 poly

--- day20.py
+++ day20.py
@@ -1,19 +1,32 @@
-import random
-r = random.randint(1,100)
-chance = 7
+def print_poly(f_x) -> str:
+    term = len(f_x) - 1
+    poly_expression = "f(x) = "
 
-while chance!=0:
-    n = int(input("Input guess number : "))
-    if n==r:
-        print("correct")
-        break
+    for i in range(len(fx)):
+        coefficient = f_x[i]
 
-    elif n>r:
-        chance = chance - 1
-        print(f"{n} > random number (chance : {chance})")
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f"{coefficient}x^{term} "
+        term = term - 1
 
-    else:
-        chance = chance - 1
-        print(f"{n} < random number(chance : {chance})")
+    return poly_expression
 
-print("end.")
+def calculation_poly(xVal, f_x):
+    return_value = 0
+    term = len(f_x) - 1  
+
+    for i in range(len(fx)):
+        coef = f_x[i]
+        return_value += coef * pow(xVal,term)
+        term = term - 1
+
+    return return_value
+
+fx = [2,3, 4, 0, -9]  # = 7x^3 -4x^2 +0x^1 +5x^0
+
+## 메인 코드 부분 ##
+if __name__ == "__main__":
+
+    print(print_poly(fx))
+    print(calculation_poly(int(input("X 값--> ")), fx))
