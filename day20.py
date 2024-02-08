0c13ee2787f6e527127f70dace14166f32441603
park020311 <zxwrtygsnm@naver.com>
2024-02-08 17:40:26 +0900

    v0.9 Node

--- day20.py
+++ day20.py
@@ -1,33 +1,98 @@
-def print_poly(f_x,t_x) -> str:
+class Node:
+    def __init__ (self) :
+        self.data = None
+        self.link = None
 
-    poly_expression = "f(x) = "
+def print_nodes(start) :
+    current = start
+    if current is None :
+        return
+    print(current.data, end=' ')
+    while current.link is not None:
+        current = current.link
+        print(current.data, end=' ')
+    print()
 
-    for i in range(len(fx)):
-        term = t_x[i]
-        coefficient = f_x[i]
-        if coefficient >= 0 and i !=0 :
-            poly_expression = poly_expression + "+"
-        poly_expression = poly_expression + f"{coefficient}x^{term} "
+def insert_node(find_data, insert_data):
+    global head, current, pre
 
+    if head.data == find_data:
+        node = Node()
+        node.data = insert_data
+        node.link = head
+        head = node
+        return
 
-    return poly_expression
+    current = head
+    while current.link is not None:
+        pre = current
+        current = current.link
+        if current.data == find_data:
+            node = Node()
+            node.data = insert_data
+            node.link = current
+            pre.link = node
+            return
 
-def calculation_poly(xVal, f_x,t_x):
-    return_value = 0
+    node = Node()
+    node.data = insert_data
+    current.link = node
+def delete_node(delete_data) :
+    global head, current, pre
 
+    if head.data == delete_data :
+        current = head
+        head = head.link
+        print(f"{delete_data}이(가) 삭제됨")
+        del(current)
+        return
 
-    for i in range(len(fx)):
-        term = t_x[i]
-        coef = f_x[i]
-        return_value += coef * pow(xVal,term)
+    current = head
+    while current.link is not None :
+        pre = current
+        current = current.link
+        if current.data == delete_data :
+            pre.link = current.link
+            print(f"{delete_data}이(가) 삭제됨")
+            del(current)
+            return
+    print(f"{delete_data}은(는) 삭제할 수 없음")
 
+def find_node(find_data) :
+    global head, current, pre
 
-    return return_value
-tx= [300,20,0]
-fx = [7,-4,5]
-#x=0일때 오류남. 상수일때 x^0 이 붙지 않게 바꿔야함..
+    current = head
+    if current.data == find_data :
+        return current
+    while current.link is not None :
+        current = current.link
+        if current.data == find_data :
+            return current
+    return Node()
 
-if __name__ == "__main__":
+head, current, pre = None, None, None
+dataArray = ["다현", "정연", "쯔위", "사나", "지효", "태간"]
 
-    print(print_poly(fx,tx))
-    print(calculation_poly(int(input("X 값--> ")), fx,tx))
+if __name__ == "__main__" :
+    node = Node()
+    node.data = dataArray[0]
+    head = node
+    for data in dataArray[1:]:
+        pre = node
+        node = Node()
+        node.data = data
+        pre.link = node
+    print_nodes(head)
+    insert_node("다현","화사")
+    print_nodes(head)
+    insert_node("사나","솔라")
+    print_nodes(head)
+    insert_node("정국","문별")
+    print_nodes(head)
+    delete_node("쯔위")
+    print_nodes(head)
+    delete_node("정국")
+    print(head)
+    print(find_node("다현").data)
+    print(find_node("재남").data)
+    print(find_node("사나").data)
