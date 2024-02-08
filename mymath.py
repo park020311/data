af1e547dde8705701e8481c3ddcde4a193e78271
park020311 <zxwrtygsnm@naver.com>
2024-02-08 10:51:34 +0900

    v0.4 combination

---
+++ mymath.py
@@ -0,0 +1,32 @@
+# def factorial(number) -> int :
+#     """
+#     factorial by repititon
+#     :param number: number(int)
+#     :return: factorial result(int)
+#     """
+#     result = 1
+#     for i in range(1, number +1):
+#         result = result * i
+#     return result
+
+def factorial(number) -> int:
+    """
+    factorial by recursion
+    :param number: number(int)
+    :return: factorial(int)
+    """
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number -1)
+
+def nCr(n,r) -> int :
+    """
+    조합 함수
+    :param n:
+    :param r:
+    :return:
+    """
+    numerator = factorial(n)
+    denominator = factorial(n-r) * factorial(r)
+    return int(numerator / denominator)
