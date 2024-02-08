155f6d1ff4da58c1ac3fc4b3851456dbd4410904
park020311 <zxwrtygsnm@naver.com>
2024-02-08 10:41:32 +0900

    v0.3 recursion preview

--- day20.py
+++ day20.py
@@ -1,9 +1,24 @@
-def factorial(number) -> int :
-    result = 1
-    for i in range(1, number +1):
-        result = result * i
-    return result
+# def factorial(number) -> int :
+#     """
+#     factorial by repititon
+#     :param number: number(int)
+#     :return: factorial result(int)
+#     """
+#     result = 1
+#     for i in range(1, number +1):
+#         result = result * i
+#     return result
 
+def factorial(number) -> int:
+    """
+    factorial by recursion
+    :param number: number(int)
+    :return: factorial(int)
+    """
+    if number == 1:
+        return 1
+    else:
+        return number * factorial(number -1)
 def nCr(n,r) -> int :
     """
     조합 함수
@@ -15,6 +30,8 @@ def nCr(n,r) -> int :
     denominator = factorial(n-r) * factorial(r)
     return int(numerator / denominator)
 if __name__ == "__main__":
-    n = int(input("Input n : "))
-    r = int(input("Input r : "))
-    print(f"{n}C{r} = {nCr(n,r)}")
+    # n = int(input("Input n : "))
+    # r = int(input("Input r : "))
+    # print(f"{n}C{r} = {nCr(n,r)}")
+    f = int(input())
+    print(factorial(f))
