e289058750b83444ade1e00b1a8aca9d6f3bf928
park020311 <zxwrtygsnm@naver.com>
2024-02-13 11:27:05 +0900

    v0.6 circular day21

--- day21.py
+++ day21.py
@@ -1,14 +1,13 @@
 import random
 
-## 클래스와 함수 선언 부분 ##
 class Node() :
-    def __init__ (self) :
+    def __init__(self):
         self.data = None
         self.link = None
 
 def print_nodes(start):
     current = start
-    if current.link is None:
+    if current.link is None :
         return
     print(current.data, end=' ')
     while current.link is not start:
@@ -16,37 +15,36 @@ def print_nodes(start):
         print(current.data, end=' ')
     print()
 
-def  count_odd_even() :
+def  count_odd_even():
     global head, current, pre
 
     odd, even = 0, 0
-    if head is None :
+    if head is None:
         return False
 
     current = head
     while True:
         if current.data % 2 == 0:
             even += 1
-        else :
+        else:
             odd += 1
-        if current.link is not head :
+        if current.link is head:
             break
         current = current.link
 
     return odd, even
 
-def make_zero_number(odd, even):
-
-    if odd > even :
+def make_minus_number(odd, even):
+    if odd > even:
         reminder = 1
-    else :
+    else:
         reminder = 0
 
     current = head
     while True:
-        if current.data % 2 is reminder:
+        if current.data % 2 == reminder:
             current.data *= -1
-        if current.link is head :
+        if current.link is head:
             break
         current = current.link
 
@@ -54,7 +52,7 @@ def make_zero_number(odd, even):
 head, current, pre = None, None, None
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
 
     data_array = [random.randint(1, 100) for _ in range(7)]
 
@@ -71,11 +69,10 @@ if __name__ == "__main__":
         pre.link = node
         node.link = head
 
-
     print_nodes(head)
 
     oddCount, evenCount = count_odd_even()
     print('홀수 -->', oddCount, '\t', '짝수 -->', evenCount)
 
-    make_zero_number(oddCount, evenCount)
+    make_minus_number(oddCount, evenCount)
     print_nodes(head)
