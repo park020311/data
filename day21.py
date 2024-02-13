79d18667878cb76da96fefa594d8b30f73829dda
park020311 <zxwrtygsnm@naver.com>
2024-02-13 13:46:22 +0900

    v0.9 stack webbrowser day21

--- day21.py
+++ day21.py
@@ -1,13 +1,32 @@
-def is_stack_empty():
+import webbrowser
+import time
+
+
+def is_stack_full():
+    global SIZE, stack, top
+    if top >= SIZE-1:
+        return True
+    else :
+        return False
+
+def is_stack_empty() :
     global SIZE, stack, top
     if top == -1:
         return True
-    else:
+    else :
         return False
+
+def push(data):
+    global SIZE, stack, top
+    if is_stack_full():
+        print("스택이 꽉 찼습니다.")
+        return
+    top += 1
+    stack[top] = data
 
 def pop():
     global SIZE, stack, top
-    if is_stack_empty():
+    if is_stack_empty() :
         print("스택이 비었습니다.")
         return None
     data = stack[top]
@@ -15,20 +34,36 @@ def pop():
     top -= 1
     return data
 
-def peek():
+def peek() :
     global SIZE, stack, top
-    if is_stack_empty():
-        print("스택이 비었습니다")
-        return
+    if is_stack_empty() :
+        print("스택이 비었습니다.")
+        return None
     return stack[top]
 
-SIZE = 5
-stack = ["커피", "사이다", "아이스티", "흑당버블밀크티", None]
-top = 3
 
-print(stack)
-retData = pop()
-print("추출한 데이터 -->", retData)
-print(stack)
-retData = pop()
-print("추출한 데이터 -->", retData)
+SIZE = 100
+stack = [ None for _ in range(SIZE) ]
+top = -1
+
+
+if __name__ == "__main__" :
+    urls = ['naver.com', 'daum.net', 'nate.com']
+
+    for url in urls :
+        push(url)
+        webbrowser.open('http://'+url)
+        print(url, end='-->')
+        time.sleep(3)
+
+    print("방문 종료")
+    time.sleep(5)
+
+    while True :
+        url = pop()
+        if url is None :
+            break
+        webbrowser.open('http://'+url)
+        print(url, end='-->')
+        time.sleep(1)
+    print("방문 종료")
