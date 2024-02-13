555eaee20e4621e3bec50fc3b7c8a243cf6a2f14
park020311 <zxwrtygsnm@naver.com>
2024-02-13 13:57:04 +0900

    v1.0 stack check bracket day21

--- day21.py
+++ day21.py
@@ -9,7 +9,7 @@ def is_stack_full():
     else :
         return False
 
-def is_stack_empty() :
+def is_stack_empty():
     global SIZE, stack, top
     if top == -1:
         return True
@@ -34,36 +34,47 @@ def pop():
     top -= 1
     return data
 
-def peek() :
+def peek():
     global SIZE, stack, top
-    if is_stack_empty() :
+    if is_stack_empty():
         print("스택이 비었습니다.")
         return None
     return stack[top]
 
+def check_bracket(expr):
+    for ch in expr:
+        if ch in '([{<':
+            push(ch)
+        elif ch in ')]}>':
+            out = pop()
+            if ch == ')' and out == '(':
+                pass
+            elif ch == ']' and out == '[':
+                pass
+            elif ch == '}' and out == '{':
+                pass
+            elif ch == '>' and out == '<':
+                pass
+            else:
+                return False
+        else:
+            pass
+    if is_stack_empty():
+        return True
+    else:
+        return False
+
 
 SIZE = 100
-stack = [ None for _ in range(SIZE) ]
+stack = [None for _ in range(SIZE)]
 top = -1
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     urls = ['naver.com', 'daum.net', 'nate.com']
 
-    for url in urls :
-        push(url)
-        webbrowser.open('http://'+url)
-        print(url, end='-->')
-        time.sleep(3)
+    exprAry = ['(A+B)', ')A+B(', '((A+B)-C', '(A+B]', '(<A+{B-C}/[C*D]>)']
 
-    print("방문 종료")
-    time.sleep(5)
-
-    while True :
-        url = pop()
-        if url is None :
-            break
-        webbrowser.open('http://'+url)
-        print(url, end='-->')
-        time.sleep(1)
-    print("방문 종료")
+    for expr in exprAry:
+        top = -1
+        print(expr, '==>', check_bracket(expr))
